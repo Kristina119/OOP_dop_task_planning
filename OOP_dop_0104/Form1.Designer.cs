096dcfb206d08cddd0981d748f50d8541289ddec
Kristina119 <lylena.br18@gmail.com>
2024-04-02 12:37:56 +0300

    Добавьте файлы проекта.

---
+++ OOP_dop_0104/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace OOP_dop_0104
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Done", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Task", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Date", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("Time", System.Windows.Forms.HorizontalAlignment.Left);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Done = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Task = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(368, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.White;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Done,
+            this.Task,
+            this.Date,
+            this.Time});
+            this.listView1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            listViewGroup1.Header = "Done";
+            listViewGroup1.Name = "Done";
+            listViewGroup2.Header = "Task";
+            listViewGroup2.Name = "Task";
+            listViewGroup3.Header = "Date";
+            listViewGroup3.Name = "Date";
+            listViewGroup4.Header = "Time";
+            listViewGroup4.Name = "Time";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2,
+            listViewGroup3,
+            listViewGroup4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 75);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(739, 338);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Done
+            // 
+            this.Done.Text = "Done";
+            this.Done.Width = 82;
+            // 
+            // Task
+            // 
+            this.Task.Text = "Task";
+            this.Task.Width = 141;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 198;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.Width = 334;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.Location = new System.Drawing.Point(615, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 28);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Done";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 29);
+            this.textBox1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.52915F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.47086F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 247F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 126F));
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 2, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(29, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(739, 38);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(635, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Done;
+        private System.Windows.Forms.ColumnHeader Task;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OOP_dop_0104/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace OOP_dop_0104
+{
+    public partial class Form1 : Form
+    {
+        private bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.CheckBoxes = true;
+            listView1.ItemCheck += listView1_ItemCheck;
+            out_file();
+        }
+        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.CurrentValue == CheckState.Checked) // Запрещаем изменение флажка CheckBox
+            {
+                e.NewValue = e.CurrentValue; 
+            }
+
+            if (e.NewValue == CheckState.Checked) 
+            {
+                foreach (ListViewItem item2 in listView1.Items)
+                {
+                        if (listView1.Items[e.Index]==item2)
+                        {
+                        item2.BackColor = Color.PaleGreen;
+                        item2.SubItems[3].Text = "done";
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                ListViewItem item = new ListViewItem();
+
+                string check = "", name = "", date = "";
+                if (checkBox1.Checked) check = "true"; 
+                else { check = "false"; flag = false; }
+                name = textBox1.Text;
+                date = dateTimePicker1.Value.ToString();
+
+                if (check == "true") item.Checked = true; 
+                    else item.Checked = false;
+
+                item.SubItems.Add(name);
+                item.SubItems.Add(date);
+                if (item.Checked == true) { item.SubItems.Add("done"); item.BackColor = Color.PaleGreen; }
+                else if (DateTime.Now > dateTimePicker1.Value) { item.SubItems.Add("olddated"); item.BackColor = Color.Tomato; }
+                else if (DateTime.Now <= dateTimePicker1.Value) item.SubItems.Add("in process");
+
+                listView1.Items.Add(item);
+            }
+                textBox1.Text = "";
+                dateTimePicker1.Value = DateTime.Now;
+                checkBox1.Checked = false;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void in_file()
+        {
+            string file_path = "in.txt";
+            if (!File.Exists(file_path))
+                File.Create(file_path).Close();
+            ListViewItem item = new ListViewItem();
+            StreamWriter writer = new StreamWriter(file_path);
+            foreach (ListViewItem item2 in listView1.Items)
+            {
+                string s = $"{item2.SubItems[1].Text},{item2.SubItems[2].Text},{item2.SubItems[3].Text}";
+                writer.WriteLine(s);
+            }
+            writer.Close();
+        }
+
+        public void out_file()
+        {
+            string file_path = "in.txt";
+            if (File.Exists(file_path))
+            {
+                StreamReader reader = new StreamReader(file_path);
+                string line= reader.ReadLine();
+                while (line!= null)
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length == 3)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        
+                        item.SubItems.Add(values[0]);
+                        item.SubItems.Add(values[1]);
+                        item.SubItems.Add(values[2]);
+
+                        DateTime date = DateTime.Parse(item.SubItems[2].Text);
+                        if (item.SubItems[3].Text == "done") { item.Checked = true; item.BackColor = Color.PaleGreen; }
+                        else if (DateTime.Now <= date) item.SubItems[3].Text = "in process";
+                        else if (DateTime.Now > date) { item.SubItems[3].Text = "olddated"; item.BackColor = Color.Tomato; }
+                        
+                        listView1.Items.Add(item);
+                    }
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item2 in listView1.Items)
+            {
+                if (item2.Selected)
+                    item2.Remove();
+            }
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            in_file();
+        }
+    }
+}
